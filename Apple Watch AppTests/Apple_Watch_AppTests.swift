43c6cd21cb5959cb2595197ad77ae15463678563
Peter Ng <png@peters-macbook-pro.local>
2015-09-23 21:00:21 -0700

    Initial Commit

---
+++ Apple Watch AppTests/Apple_Watch_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Apple_Watch_AppTests.swift
+//  Apple Watch AppTests
+//
+//  Created by Peter Ng on 9/23/15.
+//  Copyright © 2015 Peter Ng. All rights reserved.
+//
+
+import XCTest
+@testable import Apple_Watch_App
+
+class Apple_Watch_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
